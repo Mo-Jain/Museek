1d552df9c122c8743b4f16ef7a7f7fbc7300a440
Mohit Jain <dalemohit9@gmail.com>
2025-01-04 14:52:42 +0530

    adding_back_button

--- app/components/auth/auth-screen.tsx
+++ app/components/auth/auth-screen.tsx
@@ -4,18 +4,27 @@ import { useState } from "react";
 import SigninCard from "./sign-in-card";
 import SignupCard from "./sign-up-card";
 import { SignInFlow } from "@/app/types/auth-types";
+import { ArrowBigLeft, ArrowLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function AuthScreen({ authType }: { authType?: SignInFlow }) {
   const [formType, setFormType] = useState<SignInFlow>(authType || "signIn");
+  const router = useRouter(); 
   return (
     <div className=" w-full h-screen flex items-center justify-center gap-5 bg-gradient-to-b from-purple-900 to-gray-900">
-      <div className="w-full md:h-auto md:w-[420px] px-4">
-        {formType === "signIn" ? (
-          <SigninCard setFormType={setFormType} />
-        ) : (
-          <SignupCard setFormType={setFormType} />
-        )}
+        <Button variant="ghost" className="absolute top-4 left-4 text-white cursor-pointer hover:text-black"
+          onClick={() => router.back()}
+        >
+          <ArrowLeft  />
+        </Button>
+        <div className="w-full md:h-auto md:w-[420px] px-4">
+          {formType === "signIn" ? (
+            <SigninCard setFormType={setFormType} />
+          ) : (
+            <SignupCard setFormType={setFormType} />
+          )}
+        </div>
       </div>
-    </div>
   );
 }
